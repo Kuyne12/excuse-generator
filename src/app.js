853276c482e13909fee9ba6f70a2623c5fed9587
Kuyne12 <124099764+Kuyne12@users.noreply.github.com>
2023-06-15 15:53:43 +0000

    done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,30 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#theExcuse").innerHTML = generateExcuse();
+  });
+};
+
+let generateExcuse = () => {
+  let pronoun = ["a", "the", "that"];
+  let subject = ["Lion", "Sister", "Girlfriend"];
+  let action = ["smashed", "ran over", "bit"];
+  let possession = ["my foot", "her homework", "his food"];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possessionIndex = Math.floor(Math.random() * possession.length);
+
+  return (
+    pronoun[pronounIndex] +
+    " " +
+    subject[subjectIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    possession[possessionIndex] +
+    ". "
+  );
 };
